f7af47013b8666f0df223cdd10940f99af8f4e20
sidneyp321 <50895918+sidneyp321@users.noreply.github.com>
2019-05-22 23:47:46 -0300

    Adicionado FUNÇÃO "TOP"

--- IMC.py
+++ IMC.py
@@ -1,39 +1,64 @@
 import os
-nome = (input("Informe seu Nome: "))
-idade = (input("Qual sua Idade: "))
-peso = float(input("Qual seu peso. Ex: 60,80,90: "))
-altura = float(input("Qual sua altura. Ex: 1.70: "))
 
-if os.name == "nt":
-    os.system("cls")
-else:
-    os.system("clear")
 
-formula = altura * altura
-imc = peso / formula
+def top():
+    nome = (input("Informe seu Nome: "))
+    idade = (input("Qual sua Idade: "))
+    peso = float(input("Qual seu peso. Ex: 60,80,90: "))
+    altura = float(input("Qual sua altura. Ex: 1.70: "))
+    return nome, idade, peso, altura
 
-if imc < 18.5:
-    print(nome, "Seu IMC é: %.2f " % imc)
-    print("Sua Classificação é: MAGREZA")
-    print("Seu grau de obesidade é: 0")
 
-elif imc >= 18.5 and imc <= 24.9:
-    print(nome, "Seu IMC é: %.2f " % imc)
-    print("Sua Classificação é: NORMAL")
-    print("Seu grau de obesidade é: 0")
+try:
+    nome = (input("Informe seu Nome: "))
+    idade = (input("Qual sua Idade: "))
+    peso = float(input("Qual seu peso. Ex: 60,80,90: "))
+    altura = float(input("Qual sua altura. Ex: 1.70: "))
 
-elif imc >= 25.0 and imc <= 29.9:
-    print(nome, "Seu IMC é: %.2f " % imc)
-    print("Sua Classificação é: SOBREPESO")
-    print("Seu grau de obesidade é: 1")
 
-elif imc >= 30.0 and imc <= 39.9:
-    print(nome, "Seu IMC é: %.2f " % imc)
-    print("Sua Classificação é: OBESIDADE")
-    print("Seu grau de obesidade é: 2")
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
 
-else:
-    imc > 40.0
-    print(nome, "Seu IMC é: %.2f " % imc)
-    print("Sua Classificação é: OBESIDADE GRAVE")
-    print("Seu grau de obesidade é: 3")
+    formula = altura * altura
+    imc = peso / formula
+
+
+    if imc < 18.5:
+        print(nome, "Seu IMC é: %.2f " % imc)
+        print("Sua Classificação é: MAGREZA")
+        print("Seu grau de obesidade é: 0")
+
+    elif imc >= 18.5 and imc <= 24.9:
+        print(nome, "Seu IMC é: %.2f " % imc)
+        print("Sua Classificação é: NORMAL")
+        print("Seu grau de obesidade é: 0")
+
+    elif imc >= 25.0 and imc <= 29.9:
+        print(nome, "Seu IMC é: %.2f " % imc)
+        print("Sua Classificação é: SOBREPESO")
+        print("Seu grau de obesidade é: 1")
+
+    elif imc >= 30.0 and imc <= 39.9:
+        print(nome, "Seu IMC é: %.2f " % imc)
+        print("Sua Classificação é: OBESIDADE")
+        print("Seu grau de obesidade é: 2")
+
+    else:
+        imc > 40
+        print(nome, "Seu IMC é: %.2f " % imc)
+        print("Sua Classificação é: OBESIDADE GRAVE")
+        print("Seu grau de obesidade é: 3")
+
+except NameError and ValueError:
+
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+    print("USE APENAS 'NÚMEROS' e '.'")
+    top()
+
+
